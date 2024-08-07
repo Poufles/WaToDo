549d2fd997111b19ccdee26917bfabdcb539388d
Evan Ayalin <siyeel04@gmail.com>
2024-08-07 18:26:04 +0800

    Finished Reg Form Component and Added Responsiveness.

--- src/components/register/script.js
+++ src/components/register/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('button');
 const container = document.querySelector('.container.registerpage')
+const reg = document.querySelector('.registerpage .signup');
 
 btn.addEventListener('mouseup', () => {
     container.classList.remove('initial');
@@ -7,4 +8,17 @@ btn.addEventListener('mouseup', () => {
     setTimeout(() => {
         container.classList.add('noradius');
     }, 150);
+});
+
+reg.addEventListener('mouseup', () => {
+    reg.classList.add('active');
+    reg.classList.remove('pressed');
+});
+
+reg.addEventListener('mousedown', () => {
+    reg.classList.add('pressed');
+});
+
+reg.addEventListener('mouseleave', () => {
+    reg.classList.remove('pressed');
 });
